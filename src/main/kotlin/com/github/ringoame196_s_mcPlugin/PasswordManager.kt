fba7ffab560d2b18e97a72c4a9e15a228daef97a
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-08-10 14:48:18 +0900

    ✨ パスワード認証追加

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/PasswordManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/PasswordManager.kt
@@ -15,8 +15,17 @@ class PasswordManager(plugin: Plugin) {
     }
 
     fun auth(id: String, password: String): Boolean {
-        if (isLock(id)) return true
+        if (!isLock(id)) return true
         val hashPassword = hashSHA256(password)
+        val sql = "SELECT EXISTS(SELECT 1 FROM $table WHERE ${DataBaseConst.ID_KEY} = ?,${DataBaseConst.PASS_KEY} = ?) AS exists_flag;"
+        val result = dataBaseManager.acquisitionValue(sql, mutableListOf(id, hashPassword), "exists_flag")
+        return when (result) {
+            is Boolean -> result
+            is Int -> result != 0
+            is Long -> result != 0L
+            is String -> result == "1" || result.equals("true", ignoreCase = true)
+            else -> false
+        }
         return true
     }
 
