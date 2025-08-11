c20466ddd2948c985458de59a411c56ff55c1e8e
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-08-11 20:07:15 +0900

    ⬆️ Base64関係をクラス分離

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ItemStackSerializer.kt
@@ -0,0 +1,26 @@
+package com.github.ringoame196_s_mcPlugin
+
+import org.bukkit.inventory.ItemStack
+import org.bukkit.util.io.BukkitObjectInputStream
+import org.bukkit.util.io.BukkitObjectOutputStream
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.Base64
+
+object ItemStackSerializer {
+	fun itemStackToBase64(item: ItemStack): String {
+		val outputStream = ByteArrayOutputStream()
+		val dataOutput = BukkitObjectOutputStream(outputStream)
+		dataOutput.writeObject(item)
+		dataOutput.close()
+		return Base64.getEncoder().encodeToString(outputStream.toByteArray())
+	}
+
+	fun itemStackFromBase64(data: String): ItemStack {
+		val inputStream = ByteArrayInputStream(Base64.getDecoder().decode(data))
+		val dataInput = BukkitObjectInputStream(inputStream)
+		val item = dataInput.readObject() as ItemStack
+		dataInput.close()
+		return item
+	}
+}
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/ItemStorageManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ItemStorageManager.kt
@@ -1,13 +1,7 @@
 package com.github.ringoame196_s_mcPlugin
 
 import org.bukkit.inventory.Inventory
-import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.Plugin
-import org.bukkit.util.io.BukkitObjectInputStream
-import org.bukkit.util.io.BukkitObjectOutputStream
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.util.Base64
 
 class ItemStorageManager(plugin: Plugin) {
     private val dataBaseManager = DataBaseManager(plugin)
@@ -20,7 +14,7 @@ class ItemStorageManager(plugin: Plugin) {
             val parameters: List<Any>
             if (item != null) {
                 sql = "INSERT INTO $table (${DataBaseConst.ID_KEY}, ${DataBaseConst.SLOT_KEY}, ${DataBaseConst.ITEM_KEY}) VALUES (?, ?, ?) ON CONFLICT(${DataBaseConst.ID_KEY}, ${DataBaseConst.SLOT_KEY}) DO UPDATE SET ${DataBaseConst.ITEM_KEY} = excluded.${DataBaseConst.ITEM_KEY};"
-                val itemData = itemStackToBase64(item)
+                val itemData = ItemStackSerializer.itemStackToBase64(item)
                 parameters = listOf(id, slot, itemData)
             } else {
                 sql = "DELETE FROM $table WHERE ${DataBaseConst.ID_KEY} = ? AND ${DataBaseConst.SLOT_KEY} = ?;"
@@ -37,24 +31,8 @@ class ItemStorageManager(plugin: Plugin) {
         for (data in tableData) {
             val slot = data[DataBaseConst.SLOT_KEY].toString().toInt()
             val itemBase64 = data[DataBaseConst.ITEM_KEY].toString()
-            val item = itemStackFromBase64(itemBase64)
+            val item = ItemStackSerializer.itemStackFromBase64(itemBase64)
             inventory.setItem(slot, item)
         }
-    }
-
-    private fun itemStackToBase64(item: ItemStack): String {
-        val outputStream = ByteArrayOutputStream()
-        val dataOutput = BukkitObjectOutputStream(outputStream)
-        dataOutput.writeObject(item)
-        dataOutput.close()
-        return Base64.getEncoder().encodeToString(outputStream.toByteArray())
-    }
-
-    private fun itemStackFromBase64(data: String): ItemStack {
-        val inputStream = ByteArrayInputStream(Base64.getDecoder().decode(data))
-        val dataInput = BukkitObjectInputStream(inputStream)
-        val item = dataInput.readObject() as ItemStack
-        dataInput.close()
-        return item
     }
 }
