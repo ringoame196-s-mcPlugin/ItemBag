f462d254f41ef3655636d25bead1cbdfc45afdeb
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-08-11 13:57:10 +0900

    ✨ イベント実装

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/ItemBagEvents.kt
@@ -0,0 +1,30 @@
+package com.github.ringoame196_s_mcPlugin.events
+
+import com.github.ringoame196_s_mcPlugin.ItemBagManager
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.plugin.Plugin
+
+class ItemBagEvents(plugin: Plugin) : Listener {
+    private val itemBagManager = ItemBagManager(plugin)
+
+    @EventHandler
+    fun onPlayerInteract(e: PlayerInteractEvent) {
+        val player = e.player
+        val item = e.item ?: return
+        val bagID = itemBagManager.getBagID(item) ?: return
+        e.isCancelled = true
+        itemBagManager.openInv(player, bagID)
+    }
+
+    @EventHandler
+    fun onInventoryClose(e: InventoryCloseEvent) {
+        val player = e.player
+        val inventoryTitle = e.view.title
+        if (!itemBagManager.isBagInventory(inventoryTitle)) return
+        itemBagManager.closeInv(player as Player)
+    }
+}
